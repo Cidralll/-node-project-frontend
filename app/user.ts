ca475d103790b6184d0b1b03b6553ae943e6d06b
thiago-augustin <75146536+thiago-augustin@users.noreply.github.com>
2022-06-28 08:36:38 -0300

    Adicionando função de salvar dados no banco

--- app/user.ts
+++ app/user.ts
@@ -1,1 +1,58 @@
-console.log("Ta funcionando!");
+function getForm(): Object{
+    let nameUser = (<HTMLSelectElement> document.querySelector("#name")).value;
+    let cpf = (<HTMLSelectElement> document.querySelector("#cpf")).value;
+    let birthDate = (<HTMLSelectElement> document.querySelector("#birthDate")).value;
+    let email = (<HTMLSelectElement> document.querySelector("#email")).value;
+    let password = (<HTMLSelectElement> document.querySelector("#password")).value;
+    let address = (<HTMLSelectElement> document.querySelector("#address")).value;
+    let number = (<HTMLSelectElement> document.querySelector("#number")).value;
+    let complement = (<HTMLSelectElement> document.querySelector("#complement")).value;
+    let city = (<HTMLSelectElement> document.querySelector("#city")).value;
+    let state = (<HTMLSelectElement> document.querySelector("#state")).value;
+    let country = (<HTMLSelectElement> document.querySelector("#country")).value;
+    let zipCode = (<HTMLSelectElement> document.querySelector("#zipCode")).value;
+
+    let form = {
+        "name" : nameUser,
+        "cpf" : cpf,
+        "birthDate" : birthDate,
+        "email" : email,
+        "password" : password,
+        "address" : address,
+        "number" : number,
+        "complement" : complement,
+        "city" : city,
+        "state" : state,
+        "country" : country,
+        "zipCode" : zipCode
+    }
+        
+    return form;
+}
+
+const createUser = async () => {
+    let response = await createRequest();
+    let json = await response['json']();
+    let statusCode = response['status'];
+    if(statusCode == 400){
+        console.log(json['message'])
+    }else if(statusCode == 500) {
+        console.log(json['message'])
+    }else if(statusCode == 201){
+        console.log("OK!")
+    }
+}
+
+
+async function createRequest(): Promise<Object>{
+    let form = getForm();
+    console.log(JSON.stringify(form));
+    let request = await fetch("http://127.0.0.1:3000/api/v1/users", {
+        method: 'POST',
+        body: JSON.stringify(form),
+        headers: new Headers({
+            'Content-Type': 'Application/Json'
+        })
+    });
+    return request;
+}
--- dist/js/user.js
+++ dist/js/user.js
@@ -1,1 +1,66 @@
-console.log("Ta funcionando!");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function getForm() {
+    let nameUser = document.querySelector("#name").value;
+    let cpf = document.querySelector("#cpf").value;
+    let birthDate = document.querySelector("#birthDate").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+    let address = document.querySelector("#address").value;
+    let number = document.querySelector("#number").value;
+    let complement = document.querySelector("#complement").value;
+    let city = document.querySelector("#city").value;
+    let state = document.querySelector("#state").value;
+    let country = document.querySelector("#country").value;
+    let zipCode = document.querySelector("#zipCode").value;
+    let form = {
+        "name": nameUser,
+        "cpf": cpf,
+        "birthDate": birthDate,
+        "email": email,
+        "password": password,
+        "address": address,
+        "number": number,
+        "complement": complement,
+        "city": city,
+        "state": state,
+        "country": country,
+        "zipCode": zipCode
+    };
+    return form;
+}
+const createUser = () => __awaiter(this, void 0, void 0, function* () {
+    let response = yield createRequest();
+    let json = yield response['json']();
+    let statusCode = response['status'];
+    if (statusCode == 400) {
+        console.log(json['message']);
+    }
+    else if (statusCode == 500) {
+        console.log(json['message']);
+    }
+    else if (statusCode == 201) {
+        console.log("OK!");
+    }
+});
+function createRequest() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let form = getForm();
+        console.log(JSON.stringify(form));
+        let request = yield fetch("http://127.0.0.1:3000/api/v1/users", {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: new Headers({
+                'Content-Type': 'Application/Json'
+            })
+        });
+        return request;
+    });
+}
