38fc0308f9396fdbbf989e0c3dfcd27148383484
Cidrallll <lucas.woginiack@yahoo.com.br>
2022-06-28 01:27:34 -0300

    Inicio do delete

---
+++ app/connection/delete-tasks.ts
@@ -0,0 +1,14 @@
+function DeleteTask(cont) {
+    let res = confirm("You confirm delete the task")
+    if(res === true) {
+        let all = (<HTMLSelectElement><unknown>document.querySelectorAll('.div-tasks'));
+        for(let i = 0; i < all.length; i++) {
+            if(cont == all[i].id) {
+                console.log(all[i])
+            }
+        }
+    }else {
+        return
+    }
+    
+}
